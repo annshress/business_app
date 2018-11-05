ae21048258c03ae2c2c3532e20062ee0485e6148
Anish Shrestha <ann.shress@gmail.com>
2018-11-05 13:07:40 +0545

    refactor templates

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from copy import copy
 from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -9,6 +10,17 @@ class BookListView(ListView):
     template_name = "book_list.html"
     queryset = Book.objects.all().order_by('id')
     paginate_by = 2
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BookListView, self).get_context_data(*args, **kwargs)
+        if 'page' in self.request.GET:
+            request_clone = copy(self.request)
+            request_clone.GET._mutable = True
+            if len(request_clone.GET.getlist('page')) >= 1:
+                del request_clone.GET['page']
+            request_clone.GET._mutable = False
+            context['request'] = request_clone
+        return context
 
 
 class BookDetailView(DetailView):
